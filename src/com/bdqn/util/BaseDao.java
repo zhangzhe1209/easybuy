102fb20a17d20cbe20e0843decd10cd502460c5f
zhang <zhang@163.com>
2017-09-26 08:48:55 +0800

    丸

--- src/com/bdqn/util/BaseDao.java
+++ src/com/bdqn/util/BaseDao.java
@@ -3,13 +3,29 @@ package com.bdqn.util;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+/**
+ * @version 1.0
+ * @author Zeus
+ *
+ */
 public class BaseDao {
-	
+	private String name;
+
+	/**
+	 * 获得数据库连接
+	 * @return 返回连接
+	 */
 	public Connection getConnection(){
 		return null;
+	
 	}
 	
+	/**
+	 * 关闭资源
+	 * @param conn 要关闭的Connection
+	 * @param stat 要关闭的Statement
+	 * @param rs 要关闭的ResultSet
+	 */
 	public void closeAll(Connection conn,Statement stat,ResultSet rs){
 		if(conn!=null){}
 	}
